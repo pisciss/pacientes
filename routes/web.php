2c3d83a32d3750ebcc9d9c8364adff9a492d6a50
pisciss <vikinoguera@gmail.com>
2020-09-30 13:17:09 -0300

    nuevo 3

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('pacientes', 'PacienteController');
+Route::get('dataTablePaciente','PacienteController@dataTable')->name('dataTablePaciente');
